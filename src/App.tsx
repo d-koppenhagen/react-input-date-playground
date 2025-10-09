69bbfdb71ea2860d63e2a06a2ff83bc61c5c164e
d-koppenhagen <mail@k9n.dev>
2025-10-09 11:08:49 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Input type date mit React:</h1>
+
+      <h2>Variante 1: label + input</h2>
+
+      <form>
+        <label htmlFor="my-date-input-1">Bitte ein Datum eingeben:</label>
+        <input id="my-date-input-1" type="date" />
+      </form>
+
+      <h2>Variante 2: input als Kindelement von label:</h2>
+
+      <form>
+        <label>
+          Bitte ein Datum eingeben
+          <input type="date" />
+        </label>
+      </form>
+    </>
+  );
+}
+
+export default App;
